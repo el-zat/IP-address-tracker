a5706cd5d0ce6e7df5cb63340c6cdd06115099f3
Elena Zatykina <elena.zatykina@hexagon.com>
2024-08-17 11:14:21 +0200

    rendering received data

--- ip-address-tracker-master/src/index.js
+++ ip-address-tracker-master/src/index.js
@@ -6,14 +6,19 @@ const btn = document.querySelector('button')
 btn.addEventListener('click', getData)
 ipInput.addEventListener('keydown', handleKey)
 
+const ipInfo = document.querySelector('#ip')
+const locationInfo = document.querySelector('#location')
+const timezoneInfo = document.querySelector('#timezone')
+const ispInfo = document.querySelector('#isp')
+
 
 
 function getData(){
 
     if (validateIp(ipInput.value)) {
         fetch(`https://geo.ipify.org/api/v2/country?apiKey=at_HGmGYtc5clmlXfeTWrt4VyIFKA4mY&ipAddress=${ipInput.value}`)
-        .then((response) => response.json())
-        .then(console.log)
+        .then(response => response.json())
+        .then(data => setInfo(data))
 
         console.log(ipInput.value)
     }
@@ -24,5 +29,13 @@ function handleKey(event) {
     if (event.key === 'Enter') {
         getData()
     }
+}
+
+function setInfo(mapData) {
+    console.log(mapData)
+    ipInfo.innerHTML = mapData.ip
+    locationInfo.innerHTML = mapData.location.country + ", " + mapData.location.region
+    timezoneInfo.innerHTML = mapData.location.timezone
+    ispInfo.innerHTML = mapData.isp
 }
 
